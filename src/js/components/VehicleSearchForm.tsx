4f9b2ecc8073fae45582474f9de5b62d6af959a3
Kirsty <kirsty@madebysauce.com>
2025-04-15 11:42:06 +0100

    some bits before laptop swap

--- src/js/components/VehicleSearchForm.tsx
+++ src/js/components/VehicleSearchForm.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams, useLocation } from "react-router-dom";
 import { AppDispatch, RootState } from "../../store";
 import { fetchVehicles, setFilters } from "../../store/slices/vehiclesSlice";
 import Distance from "./Filter/Components/Distance";
@@ -8,6 +8,7 @@ import Distance from "./Filter/Components/Distance";
 const VehicleSearchForm = () => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
+  const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   
   const { filters, loading, totalResults } = useSelector((state: RootState) => state.vehicles);
@@ -37,13 +38,23 @@ const VehicleSearchForm = () => {
     const filtersWithPage = { ...searchFilters, page: 1 };
 
     if (shouldUpdateUrl) {
-      navigate({
-        pathname: '/search',
-        search: params.toString()
-      });
+      // If we're already on the search page, just update the params
+      if (location.pathname === '/search') {
+        setSearchParams(params);
+      } else {
+        // Otherwise, navigate to search page
+        navigate({
+          pathname: '/search',
+          search: params.toString()
+        });
+      }
     }
 
-    dispatch(fetchVehicles(filtersWithPage));
+    try {
+      await dispatch(fetchVehicles(filtersWithPage)).unwrap();
+    } catch (error) {
+      console.error('Search failed:', error);
+    }
   };
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -59,9 +70,7 @@ const VehicleSearchForm = () => {
   return (
     <div className="space-y-4">
       <form onSubmit={handleSubmit} className="grid gap-6 p-6 bg-white rounded-lg shadow">
-
         <Distance />
-
         <button
           type="submit"
           disabled={loading}
@@ -71,11 +80,11 @@ const VehicleSearchForm = () => {
         </button>
       </form>
 
-      {/* {totalResults > 0 && (
+      {totalResults > 0 && (
         <div className="p-4 bg-gray-50 rounded">
           <p className="text-gray-700">{totalResults} vehicles found</p>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
