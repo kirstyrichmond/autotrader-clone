a971b8c247b3f93bd8740d8450ee69bcbb80e775
Kirsty Richmond <kirsty@madebysauce.com>
2025-07-19 23:55:37 +0100

    Update for PORT variable

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app, db
 
 app = create_app()
@@ -5,4 +6,5 @@ app = create_app()
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        app.run(debug=True)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port, debug=False)
